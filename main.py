f1efd1caac788cf9233c85d5b37b6b419f9a92b8
timkib <92236550+timkib@users.noreply.github.com>
2023-09-21 10:39:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -61,66 +61,68 @@ if option_type == "Barrier option":
     barrier_H = st.number_input('Enter barrier', min_value=0.1)
     option_pricing_BS.H = barrier_H
 
+try:
+    if st.button('Calculate price'):
+        if excercise_type == "European style" and model_type == "Black-Scholes market":
+            if option_type == "Vanilla option":
+                if solving_method == "Explicit formula":
+                    V_0_call = option_pricing_BS.BlackScholes_EuCall() # call option
+                    V_0_put = option_pricing_BS.BlackScholes_EuPut() # put option
+                elif solving_method == "Monte-Carlo":
+                    V_0_call = option_pricing_BS.Eu_Option_BS_MC("Call") # call option
+                    V_0_put = option_pricing_BS.Eu_Option_BS_MC("Put") # put option
+                elif solving_method == "PDE-method":
+                    stock_matrix, option_price_matrix  = option_pricing_BS.Eu_Option_BS_PDE() # call option
+                    idx = np.absolute(stock_matrix-S_t).argmin()
+                    V_0_call = option_price_matrix[idx]
+                    V_0_put = 0 # put option  #TODO
+                elif solving_method == "Integral transformation":
+                    V_0_call  = option_pricing_BS.Eu_Option_BS_LaPlace("Call") # call option
+                    V_0_put = option_pricing_BS.Eu_Option_BS_LaPlace("Put") # put option 
 
-if st.button('Calculate price'):
-    if excercise_type == "European style" and model_type == "Black-Scholes market":
-        if option_type == "Vanilla option":
-            if solving_method == "Explicit formula":
-                V_0_call = option_pricing_BS.BlackScholes_EuCall() # call option
-                V_0_put = option_pricing_BS.BlackScholes_EuPut() # put option
-            elif solving_method == "Monte-Carlo":
-                V_0_call = option_pricing_BS.Eu_Option_BS_MC("Call") # call option
-                V_0_put = option_pricing_BS.Eu_Option_BS_MC("Put") # put option
-            elif solving_method == "PDE-method":
-                stock_matrix, option_price_matrix  = option_pricing_BS.Eu_Option_BS_PDE() # call option
-                idx = np.absolute(stock_matrix-S_t).argmin()
-                V_0_call = option_price_matrix[idx]
-                V_0_put = 0 # put option  #TODO
-            elif solving_method == "Integral transformation":
-                V_0_call  = option_pricing_BS.Eu_Option_BS_LaPlace("Call") # call option
-                V_0_put = option_pricing_BS.Eu_Option_BS_LaPlace("Put") # put option 
+            elif option_type == "Barrier option":
+                if solving_method == "Explicit formula":
+                    V_0_call = option_pricing_BS.BlackScholes_Eu_Barrier_Call() # call option
+                    V_0_put = option_pricing_BS.BlackScholes_Eu_Barrier_Put() # put option
+                elif solving_method == "Monte-Carlo":
+                    V_0_call = option_pricing_BS.Eu_Option_BS_MC_Barrier("Call") # call option
+                    V_0_put = option_pricing_BS.Eu_Option_BS_MC_Barrier("Put") # put option
 
-        elif option_type == "Barrier option":
-            if solving_method == "Explicit formula":
-                V_0_call = option_pricing_BS.BlackScholes_Eu_Barrier_Call() # call option
-                V_0_put = option_pricing_BS.BlackScholes_Eu_Barrier_Put() # put option
-            elif solving_method == "Monte-Carlo":
-                V_0_call = option_pricing_BS.Eu_Option_BS_MC_Barrier("Call") # call option
-                V_0_put = option_pricing_BS.Eu_Option_BS_MC_Barrier("Put") # put option
-
-    elif excercise_type == "European style" and model_type == "Heston model":
-        if option_type == "Vanilla option":
-            if solving_method == "Explicit formula":
-                st.warning('There is no closed form solution. Try Monte-Carlo method.')
-                V_0_call = 0 # call option
-                V_0_put = 0 # put option
-            elif solving_method == "Monte-Carlo":
-                V_0_call = option_pricing_Heston.Heston_EuCall_MC_Euler("Call") # call option
-                V_0_put = option_pricing_Heston.Heston_EuCall_MC_Euler("Put") # put option
-            elif solving_method == "Integral transformation":
-                V_0_call = option_pricing_Heston.Heston_EuCall_MC_LaPlace("Call") # call option
-                V_0_put = option_pricing_Heston.Heston_EuCall_MC_LaPlace("Put") # put option
-    
-    elif excercise_type == "American style" and model_type == "Black-Scholes market":
-        if solving_method == "PDE-method":
-            stock_matrix_call, option_price_matrix_call  = option_pricing_BS.Am_Option_BS_PDE("Call") # call option
-            idx = np.absolute(stock_matrix_call-S_t).argmin()
-            V_0_call = option_price_matrix_call[idx]
-
-            stock_matrix_put, option_price_matrix_put  = option_pricing_BS.Am_Option_BS_PDE("Put") # call option
-            idx = np.absolute(stock_matrix_put-S_t).argmin()
-            V_0_put = option_price_matrix_put[idx]
+        elif excercise_type == "European style" and model_type == "Heston model":
+            if option_type == "Vanilla option":
+                if solving_method == "Explicit formula":
+                    st.warning('There is no closed form solution. Try Monte-Carlo method.')
+                    V_0_call = 0 # call option
+                    V_0_put = 0 # put option
+                elif solving_method == "Monte-Carlo":
+                    V_0_call = option_pricing_Heston.Heston_EuCall_MC_Euler("Call") # call option
+                    V_0_put = option_pricing_Heston.Heston_EuCall_MC_Euler("Put") # put option
+                elif solving_method == "Integral transformation":
+                    V_0_call = option_pricing_Heston.Heston_EuCall_MC_LaPlace("Call") # call option
+                    V_0_put = option_pricing_Heston.Heston_EuCall_MC_LaPlace("Put") # put option
         
-        elif solving_method == "Monte-Carlo":
-            V_0_call = option_pricing_BS.Am_Option_BS_LS("Call") # call option
-            V_0_put = option_pricing_BS.Am_Option_BS_LS("Put") # put option
+        elif excercise_type == "American style" and model_type == "Black-Scholes market":
+            if solving_method == "PDE-method":
+                stock_matrix_call, option_price_matrix_call  = option_pricing_BS.Am_Option_BS_PDE("Call") # call option
+                idx = np.absolute(stock_matrix_call-S_t).argmin()
+                V_0_call = option_price_matrix_call[idx]
 
-    elif excercise_type == "American style" and model_type == "Heston model":
-        if solving_method == "Monte-Carlo":
-            V_0_call = option_pricing_Heston.Am_Option_Heston_LS("Call") # call option
-            V_0_put = option_pricing_Heston.Am_Option_Heston_LS("Put")  # put option 
-        
-    st.text(f"Call price: {np.round(V_0_call, 2)} | Put price: {np.round(V_0_put, 2)}")
+                stock_matrix_put, option_price_matrix_put  = option_pricing_BS.Am_Option_BS_PDE("Put") # call option
+                idx = np.absolute(stock_matrix_put-S_t).argmin()
+                V_0_put = option_price_matrix_put[idx]
+            
+            elif solving_method == "Monte-Carlo":
+                V_0_call = option_pricing_BS.Am_Option_BS_LS("Call") # call option
+                V_0_put = option_pricing_BS.Am_Option_BS_LS("Put") # put option
+
+        elif excercise_type == "American style" and model_type == "Heston model":
+            if solving_method == "Monte-Carlo":
+                V_0_call = option_pricing_Heston.Am_Option_Heston_LS("Call") # call option
+                V_0_put = option_pricing_Heston.Am_Option_Heston_LS("Put")  # put option 
+            
+        st.text(f"Call price: {np.round(V_0_call, 2)} | Put price: {np.round(V_0_put, 2)}")
+except:
+   st.text("Try another method.") 
 
 
 st.header('How does it work?')
@@ -149,7 +151,7 @@ st.latex(r'''
  } 
     ''')
 st.write('S_0 denotes the prices the bond for discounting, tau is the optimal stopping time giving the natural filtration F_t.')
-st.write("All calculations are based on my OptionPricing package, for more information see my github [link](https://github.com/timkib/OptionPricing)")
 st.write('Heston model: ')
 st.write('The main difference is that in the heston model, the volatility is not assumed to constant but replaced by a stochastic process. This is motivated by the fact that in real markets the volatility of an asset is not constant.')
+st.write("All calculations are based on my OptionPricing package, for more information see my github [link](https://github.com/timkib/OptionPricing)")
 
